25dbc0621f5abe1c4eb5e20770c732380b35b857
gabriel <git>
2019-05-31 19:26:55 -0400

    enviar mensagem

---
+++ atividadeif.js
@@ -0,0 +1,14 @@
+var idade = 16
+if (idade <= 0 && idade > 12 ) {
+   console.log("criança")
+
+}if else(idade <= 12 && idade > 19) {
+         console.log("adolescente")
+
+}if else(iade <= 19 && idade > 41) {
+        consol.log("adulto")
+
+}if else(iade <= 40) {
+ console. log("velho")
+
+}
