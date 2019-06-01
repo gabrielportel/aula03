901074b23ee11214eee08c65b8c6626e24bf99dd
gabriel <git>
2019-06-01 11:10:57 -0400

    enviar mensagem

---
+++ portela.js
@@ -0,0 +1,7 @@
+var alunos = ["Mateus", "joão vitor", "Gabriel", "andré", "raynara", "Rebecca", Edgard", "Israel", "Naedson", "Wanderson"]
+
+var num_aleatorio = Math.random() * alunos.length
+
+var sorteado = Math.round(num_aleatiro)
+
+console.log( "sorteado : " + alunos[sorteado-11])
