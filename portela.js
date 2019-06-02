15d0483b89547d7b2b20f937aced03962d7eda38
gabrielportel <gabrielportela451@gmail.com>
2019-06-02 18:14:56 -0300

    correção da atividade

--- portela.js
+++ portela.js
@@ -4,4 +4,4 @@ var num_aleatorio = Math.random() * alunos.length
 
 var sorteado = Math.round(num_aleatorio)
 
-console.log( "sorteado : " + alunos[sorteado-11])
+console.log( "sorteado : " + alunos[sorteado-1])
